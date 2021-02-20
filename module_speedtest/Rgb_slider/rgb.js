81ea63fdf8609de4d00ae582636552c776caf928
safitri02 <safitri1337@gmail.com>
2021-02-20 10:01:46 +0700

    Doing task module speedtest

---
+++ module_speedtest/Rgb_slider/rgb.js
@@ -0,0 +1,49 @@
+const red = document.getElementById('red')
+const green = document.getElementById('green')
+const blue = document.getElementById('blue')
+const hasil_warna = document.getElementById('hasil_warna')
+
+ubahWarna()
+
+function ubahWarna(){
+    const merah = parseInt(red.value,10).toString(16);
+    const hijau = parseInt(green.value,10).toString(16);
+    const biru = parseInt(blue.value,10).toString(16);
+
+    //ganti background
+    const hex_color = '#'+pad(merah)+pad(hijau)+pad(biru);
+    hasil_warna.style.backgroundColor = hex_color
+
+    //mengambil value
+    const rgb_hex = `rgb(${red.value}, ${green.value}, ${blue.value})`;
+    hasil_warna.innerText= rgb_hex
+    
+}
+
+function pad(n){
+    //namanya ternary function
+    //jika kurang dari 2 maka tambahkan 0 di depannya
+    return (n.length>2) ? "0"+n:n; 
+}
+
+red.addEventListener('change', function(){
+    ubahWarna()
+})
+red.addEventListener('input', function(){
+    ubahWarna()
+})
+
+green.addEventListener('change', function(){
+    ubahWarna()
+})
+green.addEventListener('input', function(){
+    ubahWarna()
+})
+
+blue.addEventListener('change', function(){
+    ubahWarna()
+})
+blue.addEventListener('input', function(){
+    ubahWarna()
+})
+
---
+++ module_speedtest/php_chapta/index.php
@@ -0,0 +1,23 @@
+<?php  
+
+session_start();
+
+if(isset($_POST['submit']) and $_SESSION['captcha'] == $_POST['text_captcha']){
+
+	echo "<script> alert('Selamat, anda lolos captcha!') </script>";
+
+}else{
+
+	echo "<script> alert('Captcha salah, silahkan ulangi lagi') </script>";
+
+}
+
+
+?>
+
+<form method="post">
+	 <p> Kode Captcha : </p>
+	 <p> <img src="captcha.php"> </p> <br>
+	 <input type="text" name="text_captcha" maxlength="6"> <br>
+	 <input type="submit" name="submit" value="Submit">
+</form>
